bf921d9ab45d97b95135a76f9525f0985ae6a0f5
zsoltc <varazsgomba@freemail.hu>
2014-12-25 22:30:05 +0100

    Added gradient generator

---
+++ bin/gradient-generator.dev.js
@@ -0,0 +1,27 @@
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var n;"undefined"!=typeof window?n=window:"undefined"!=typeof global?n=global:"undefined"!=typeof self&&(n=self),n.GradientGenerator=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+function createGradient(colorStops) {
+    return new Gradient(colorStops);
+}
+
+function Gradient() {
+
+}
+
+Gradient.prototype.getColorAt = function (value) {
+    return {
+        r: value,
+        g: value,
+        b: value
+    };
+};
+
+function lerp(a, b, t) {
+    return a + t * (b - a);
+}
+
+module.exports = {
+    createGradient: createGradient
+};
+
+},{}]},{},[1])(1)
+});
---
+++ src/gradient-generator.js
@@ -0,0 +1,23 @@
+function createGradient(colorStops) {
+    return new Gradient(colorStops);
+}
+
+function Gradient() {
+
+}
+
+Gradient.prototype.getColorAt = function (value) {
+    return {
+        r: value,
+        g: value,
+        b: value
+    };
+};
+
+function lerp(a, b, t) {
+    return a + t * (b - a);
+}
+
+module.exports = {
+    createGradient: createGradient
+};
