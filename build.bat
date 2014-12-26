ec22d566a1378354b118630329a9ecace8b99be4
zsoltc <zsoltc@users.noreply.github.com>
2014-12-26 19:04:00 +0100

    Added minified version

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-browserify src/gradient-generator.js --standalone GradientGenerator > bin/gradient-generator.dev.js
+call browserify src/gradient-generator.js --standalone GradientGenerator > bin/gradient-generator.dev.js
+call uglifyjs bin/gradient-generator.dev.js -o bin/gradient-generator.js -c -m
