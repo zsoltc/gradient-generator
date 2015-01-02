4247e13a2776601e0c7c6523db84a2c3468b0d47
Zsolt Czinege <zsoltc@users.noreply.github.com>
2015-01-02 03:30:42 +0100

    Changed noise example

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 call browserify src/gradient-generator.js --standalone GradientGenerator > bin/gradient-generator.dev.js
-call uglifyjs bin/gradient-generator.dev.js -o bin/gradient-generator.js -c -m
+call uglifyjs bin/gradient-generator.dev.js -o bin/gradient-generator.min.js -c -m
