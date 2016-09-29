fe3b791beb2da789f33d09fde44cd407fc05b3b2
Zsolt Czinege <zsoltc@users.noreply.github.com>
2016-09-30 00:36:35 +0200

    Setting up build

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,37 @@
-var webpack = require('webpack');
 var path = require('path');
-var env = require('yargs').argv.mode;
+var webpack = require('webpack');
 var libraryName = 'gradient-generator';
-var plugins = [];
-var outputFile;
+var globalName = 'GradientGenerator';
+var paths = {
+  src: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist')
+};
 
-if (env === 'build') {
-  plugins.push(new webpack.optimize.UglifyJsPlugin({ minimize: true }));
-  outputFile = libraryName + '.min.js';
-} else {
-  outputFile = libraryName + '.js';
-}
-
-var config = {
-  entry: __dirname + '/src/' + libraryName + '.js',
-  devtool: 'source-map',
+module.exports = {
+  entry: path.join(paths.src, libraryName + '.js'),
   output: {
-    path: __dirname + '/dist',
-    filename: outputFile,
-    library: libraryName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true
+    path: paths.dist,
+    filename: libraryName + '.js',
+    library: globalName,
+    libraryTarget: 'umd'
   },
   module: {
     preLoaders: [
       {
         test: /\.jsx?$/,
-        loader: "eslint-loader",
-        include: __dirname + '/src'
+        loader: 'eslint-loader',
+        include: paths.src,
       }
     ],
     loaders: [
       {
         test: /\.jsx?$/,
-        loader: 'babel',
-        include: __dirname + '/src'
+        loader: 'babel-loader',
+        include: paths.src,
+        query: {
+          presets: ['es2015']
+        }
       }
     ]
   },
-  resolve: {
-    root: path.resolve('./src'),
-    extensions: ['', '.js', '.jsx']
-  },
-  plugins: plugins
 };
-
-module.exports = config;
