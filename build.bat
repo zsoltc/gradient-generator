ab914ff53d0fe6011c9aea61fe0e8bd4f8ab7980
Zsolt Czinege <zsoltc@users.noreply.github.com>
2016-09-29 22:12:54 +0200

    Added package.json

--- build.bat
+++
@@ -1,2 +0,0 @@
-call browserify src/gradient-generator.js --standalone GradientGenerator > bin/gradient-generator.dev.js
-call uglifyjs bin/gradient-generator.dev.js -o bin/gradient-generator.min.js -c -m
